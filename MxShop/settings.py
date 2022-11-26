2f0bff0aa243f966885e1cb6159e09260cb5ef73
apollo <apollo@163.com>
2022-11-26 17:10:22 +0800

    添加media url

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = "users.UserProfile"
 
+MEDIA_ROOT = "media"
 
 # Application definition
 
--- MxShop/urls.py
+++ MxShop/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from MxShop.settings import MEDIA_ROOT
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ueditor/', include('DjangoUeditor.urls'))
+    path('ueditor/', include('DjangoUeditor.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT})
 ]
